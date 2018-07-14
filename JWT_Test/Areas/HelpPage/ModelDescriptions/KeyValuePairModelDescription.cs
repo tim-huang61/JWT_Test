919542f76906ec4a92fe3fda4e6ec3c82edab9f0
tim <tim.huang61@gmail.com>
2018-07-14 20:15:08 +0800

    project init

---
+++ JWT_Test/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace JWT_Test.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ JWT_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JWT_Test.WebApiApplication" Language="C#" %>
